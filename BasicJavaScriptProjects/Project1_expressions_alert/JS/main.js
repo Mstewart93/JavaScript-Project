560ca525652b8ffd56ee8da484a2b8a43fa6f554
Mstewart93 <121573726+Mstewart93@users.noreply.github.com>
2023-03-04 17:13:54 -0700

    update

---
+++ BasicJavaScriptProjects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,34 @@
+alert("Hello World\n")  ; //this pops up alone like this//
+
+window.alert("This is a pop up"); //This also pops up too the same way//
+
+document.write('This is a different way to do this as well.'); //This shows up as part of the page itself.//
+
+var A = "\n This is a string using a variable to exacute."; //This assigns a variable//
+
+document.write(A); //this will exacute the variable//
+
+document.write("Lisa told Bart, \"Knock it off Bart! Or I will tell dad!\" ");
+
+// a\ is called escaping a character, it shows that the thing that follows is special.//
+
+document.write("\"Be who you are and say what you feel," +  "because those who mind don\'t matter and those who matter don\'t mind.\”"+ "-Dr. Seuss");
+
+//this is to concatenate or connect strings together.//
+
+var B = "\"Be who you are and say what you feel," +  "because those who mind don\'t matter and those who matter don\'t mind.\”"+ "-Dr. Seuss" + "Hello"
+
+document.write(B)
+
+//Using variables and Concatenate//
+
+var A = "Mother", B = "Father", C = "Sister", D = "Brother", E = "Son", F = "Daughter";
+
+document.write(C);
+
+ var sent1 = "This is the beginning of the string";
+ var sent2 = " and This is the end of the string";
+
+ document.write(sent1 + sent2);
+
+ document.write(4*28798);
