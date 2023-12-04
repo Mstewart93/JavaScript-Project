b247e8e09fc469a612a919d03d87217eee4cdce0
Mstewart93 <121573726+Mstewart93@users.noreply.github.com>
2023-12-04 06:36:37 -0700

    Update basics.js

--- Zero to Mastery/basics.js
+++ Zero to Mastery/basics.js
@@ -1,2 +1,111 @@
-// This is a refresher of the basics of JavaScript from Zero to Mastery next is intermediate  then we are looking at 
+// This is a refresher of the basics of JavaScript from Zero to Mastery next is intermediate  then we are looking at advanced concepts
+
+//like many  programming lanugages there are TYPES
+
+//number
+3+4
+3*4
+3/4
+3-4
+(20-5) * 15
+12 % 4 //this will give you the remainder so since 12 divided by 4 is 3 with no remainder the answer would be 0.
+
+//string
+"This is a string."
+'This is also a string.'
+"Hello" + " " + "there"
+//if you try to add text to string it wont add in a traditional sense of the word
+10 + "35" // this would give you 1035 instead of 45 like you might assume it would. it converst by itself the 10 to a string
+10 - "35" //will solve it because a minus is not related to strings it is only related to numbers it will change the 35 to number
+//best to keep actions involving types of the same kind 
+
+//boolean
+true
+false
+//boolean has only two answers it is either true or false , 1 or 0  on or off. 
+3 > 2 // would give us back true as 3 is larger than 2.
+
+//undefinded
+
+//null
+
+//symbol 
+
+//object
+
+
+//We also have COMPARISINS that you will see in many languages 
+
+//!==
+
+//===
+
+//<=
+
+//>=
+
+//<
+
+//>
+
+
+// We also have VARIABLES that we can call
 var cat = 2 
+// we can also turn a string into a number say
+var dog = two
+Number(dog)
+//this is particularily usefull when taking user input.
+
+//let
+
+//const
+
+//JavaScript CONDITIONALS
+
+//if
+
+//else 
+
+//else if
+
+//ternary operator
+
+//switch
+
+//LOGICAL OPERATORS
+
+//&&
+
+//||
+
+// !
+
+//FUNCTIONS
+
+//var a = function name() {}
+
+// function name() {}
+
+//return
+
+// () =>
+
+
+//DATA STRUCTURES
+
+//Array
+
+// Object
+
+//Javascript LOOPING
+
+//for
+
+//while
+
+//do
+
+//for each
+
+//KEYWORDS
+
